c3d7996c789646559dde5974d19dfef226545c98
IlyaGrishkin <ilagriskin5@gmail.com>
2025-10-10 13:04:18 +0400

    small fix

--- cyberforum/moderation/forms.py
+++ cyberforum/moderation/forms.py
@@ -18,11 +18,11 @@ class CourseForm(forms.ModelForm):
 class LessonForm(forms.ModelForm):
     class Meta:
         model = Lesson
-        fields = ['title', 'content', 'video_url', 'order']
+        fields = ['title', 'content', 'video_file', 'order']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 6}),
-            'video_url': forms.URLInput(attrs={'class': 'form-control'}),
+            'video_file': forms.FileInput(attrs={"class": "form-control", "accept": "video/*"}),
             'order': forms.NumberInput(attrs={'class': 'form-control', 'min': 1}),
         }
 
